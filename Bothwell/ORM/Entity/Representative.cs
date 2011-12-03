433372a392b842bedacfcff59aebe58d9bced980
hainesmc <haines2m@gmail.com>
2011-12-03 12:01:52 -0600

    Added representative to ORM folder

---
+++ Bothwell/ORM/Entity/Representative.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bothwell.ORM.Entity
+{
+	public class Representative
+	{
+		public virtual int RepresentativeID { get; private set; }
+		public virtual string Name { get; set; }
+		public virtual string ImageUrl { get; set; }
+
+		public virtual string ContactInformation { get; set; }
+		public virtual string ShapeData { get; set; }
+		
+	}
+}
---
+++ Bothwell/ORM/Mapping/RepresentativeMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bothwell.ORM.Entity;
+using FluentNHibernate.Mapping;
+
+namespace Bothwell.ORM.Mapping
+{
+	public class RepresentativeMap : ClassMap<Employee>
+	{
+		public RepresentativeMap()
+		{
+			Id(x => x.Id);
+			Map(x => x.Name);
+			Map(x => x.IsCool);
+		}
+	}
+}
